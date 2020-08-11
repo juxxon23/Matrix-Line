901fbe863696e009c1c86cc84e3eb08ff5f32d1f
DarkMatter <juxxon23@hotmail.com>
2020-08-11 15:29:27 -0500

    Cambios menores

--- src/app/components/line/line.component.ts
+++ src/app/components/line/line.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-line',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line.component.css']
 })
 export class LineComponent implements OnInit {
+  signin = this.fb.group({
+	nome: [''],
+    lastName: [''],
+    id: [''],
+    numberPhone: [''],
+    date: ['']
+	});
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
