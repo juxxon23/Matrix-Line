d562d077abd22b1e792dc018918fdcc43d5e9e72
DarkMatter <juxxon23@hotmail.com>
2020-08-11 02:00:49 -0500

    Login con formularios reactivos

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+	login = this.fb.group({
+		user: [''],
+		pass: ['']
+	});
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  	constructor(private fb: FormBuilder) { }
+	
 }
