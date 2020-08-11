2c24e776921e669bc77bbfdf4a7c91aab112a47b
nelson.pi <nextnelson1234@gmail.com>
2020-08-11 13:15:17 -0500

    modificado

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class SigninComponent {
+  signin = this.fb.group({
+		nome: [''],
+    lastName: [''],
+    id: [''],
+    numberPhone: [''],
+    date: ['']
+	});
+  constructor(private fb: FormBuilder) { }
 
 }
