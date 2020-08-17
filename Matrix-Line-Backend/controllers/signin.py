ecbc1e0a1cafe0a207a6f9582485cb3e04f3d595
DarkMatter <juxxon23@hotmail.com>
2020-08-17 13:15:17 -0500

    Signin back completo

--- Matrix-Line-Backend/controllers/signin.py
+++ Matrix-Line-Backend/controllers/signin.py
@@ -7,34 +7,16 @@ data_m = DataManager()
 
 class Signin(MethodView):
     def post(self):
-        asesor_signin = request.get_json()
-        new_asesor = Asesor(
-               nombre_a=asesor_signin['name'],
-               apellido_a=asesor_signin['lastName'],
-               documento_a=asesor_signin['id'],
-               password_a=asesor_signin['pass'],
-               telefono_a=asesor_signin['numberPhone'])
-        state = data_m.add(new_asesor)
-        return jsonify({'state':state})
-        '''
-        #se almacena en un lista json lo que llega por post
-        userNew = {
-            "name":request.json['name'],
-            "apellido":request.json['apellido'],
-            "telefono": request.json['telefono'],
-            "documento": request.json['documento'],
-            "password" : request.json['password']
-        }
-
-        users.append(userNew)
-
-        name = request.json['name']
-
-        userFound = [ users for users in users if users['name'] == name ]
-        # se verifica que si este el producto si existe si la longitud de la lista es mayor a 0 es que existe
-        if( len(userFound) > 0):
-            return jsonify({"message":"register succesfulluy" })
-
-        return jsonify({
-            "message": "register fail"
-        })'''
+        try:
+            asesor_signin = request.get_json()
+            new_asesor = Asesor(
+                    nombre_a=asesor_signin['name'],
+                    apellido_a=asesor_signin['lastName'],
+                    documento_a=asesor_signin['id'],
+                    password_a=asesor_signin['pass'],
+                    telefono_a=asesor_signin['numberPhone'])
+            state = data_m.add(new_asesor)
+            return jsonify({'state':state})
+        except:
+            return jsonify({'state':state})
+  
