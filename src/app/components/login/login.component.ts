e166c9c4da6988f19b7b0da6b5a8a27193d15939
DarkMatter <juxxon23@hotmail.com>
2020-08-17 12:54:05 -0500

    Login front completo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -24,28 +24,42 @@ export class LoginComponent {
 
 	url_login : string = 'http://127.0.0.1:5000/login';	
 	dataEx : JSON;
+	state : string;	
 	onSubmit() {
+		/* Post */
+		this.rs.postData(this.url_login,this.login_client.value).subscribe(data => {
+			this.dataEx = data as JSON;
+			this.state = this.dataEx['state'];
+			switch(this.state) {
+				case 'welcome': {
+					this.router.navigate(['/'], {relativeTo: this.route});
+					console.log('Welcome');
+					break;
+				} case 'password': {
+					console.log('Incorrect Password');
+					break;
+				} case 'document': {
+					console.log('Incorrect id');
+					break;
+				} default: {
+					console.log('Error');
+				}
+			}
+		});
+
 		/* Metodo delete
 		   this.rs.deleteData(this.url_login, this.login_client.get('pass').value).subscribe(data => {
 			this.dataEx = data as JSON;
 			console.log(this.dataEx);
-		});
-		*/
+		});*/
+
 		/* Metodo put
 		   this.rs.updateData(this.url_login, this.login_client.value).subscribe(data => {
 	this.dataEx = data as JSON;
 	console.log(this.dataEx);
 		});*/
-		/* Metodo post
-		this.rs.postData(this.url_login,this.login_client.value).subscribe(data => {
-			this.dataEx = data as JSON;
-			if(this.dataEx['state'] == 'welcome') {
-				this.router.navigate(['/'], {relativeTo: this.route});
-			} else {
-				console.log(this.dataEx);
-			}
-		});*/
-	   /* Metodo get
+
+		/* Metodo get
 	   	  this.rs.getData(this.url).subscribe(data => {
 	   	  	this.dataEx = data as JSON;
 	   	  	console.log(this.dataEx);
