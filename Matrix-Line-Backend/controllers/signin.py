9e72eee7271eef3926dd4b7420798055963a2e71
DarkMatter <juxxon23@hotmail.com>
2020-08-17 14:23:53 -0500

    Ajustes

--- Matrix-Line-Backend/controllers/signin.py
+++ Matrix-Line-Backend/controllers/signin.py
@@ -6,15 +6,16 @@ from helpers.data_manager import DataManager
 data_m = DataManager()
 
 class Signin(MethodView):
+
     def post(self):
         try:
             asesor_signin = request.get_json()
             new_asesor = Asesor(
-                    nombre_a=asesor_signin['name'],
-                    apellido_a=asesor_signin['lastName'],
-                    documento_a=asesor_signin['id'],
-                    password_a=asesor_signin['pass'],
-                    telefono_a=asesor_signin['numberPhone'])
+                    nombre_a = asesor_signin['name'],
+                    apellido_a = asesor_signin['lastName'],
+                    documento_a = asesor_signin['id'],
+                    password_a = asesor_signin['pass'],
+                    telefono_a = asesor_signin['numberPhone'])
             state = data_m.add(new_asesor)
             return jsonify({'state':state})
         except:
