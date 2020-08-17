c9db3b70a9b21aa1ce1e775fffa89e3e51867e2c
DarkMatter <juxxon23@hotmail.com>
2020-08-17 14:23:11 -0500

    Registro usuario back completo

--- Matrix-Line-Backend/controllers/user.py
+++ Matrix-Line-Backend/controllers/user.py
@@ -1,9 +1,23 @@
 from flask import jsonify, request
 from flask.views import MethodView
-from data.baseDatos import users
+from data.model import Usuario 
+from helpers.data_manager import DataManager
 
+data_m = DataManager()
 
 class User(MethodView):
 
-    def funcname(self):
-        pass
+    def post(self):
+        try:
+            usuario_reg = request.get_json()
+            new_usuario = Usuario(
+                    nombre_u = usuario_reg['name'],
+                    apellido_u = usuario_reg['lastName'],
+                    documento_u = usuario_reg['id'],
+                    telefono_u = usuario_reg['numberPhone'],
+                    fecha_nacimiento = usuario_reg['date'])
+            state = data_m.add(new_usuario)
+            return jsonify({'state':state})
+        except:
+            return jsonify({'state':state})
+  
