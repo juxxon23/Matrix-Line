439a51e8779318538b35219efac38b21e6ce74e2
nelson <nextnelson1234@gmail.com>
2020-09-07 12:14:30 -0500

    na

---
+++ Matrix-Line-Backend/controllers/consultUser.py
@@ -0,0 +1,15 @@
+from flask.views import MethodView
+from flask import jsonify,request
+from data.model import Usuario
+
+class ConsultUser (MethodView):
+
+    def post(self):
+        dataEx = request.get_json()
+        data = Usuario.query.filter_by(documento_u=dataEx['document']).first()
+        if data != None:
+            return jsonify({'state':'welcome'}) 
+        else:
+            return jsonify({'state':'document'})
+        return 'complete', 200
+            
