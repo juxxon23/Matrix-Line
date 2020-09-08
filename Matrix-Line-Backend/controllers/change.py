1df385e72f8786fea1511908d77a9b6245166f70
DarkMatter <juxxon23@hotmail.com>
2020-09-08 15:05:46 -0500

    Metodo put completo

--- Matrix-Line-Backend/controllers/change.py
+++ Matrix-Line-Backend/controllers/change.py
@@ -2,6 +2,7 @@ from flask.views import MethodView
 from flask import json, request, jsonify
 from data.model import Usuario, Linea
 from helpers.data_manager import DataManager
+from sqlalchemy import table
 
 data_m = DataManager()
 
@@ -9,7 +10,7 @@ class Change(MethodView):
 
     def post(self):
         dataEx = request.get_json()
-        data = Usuario.query.filter_by(documento_u=dataEx['document'])
+        data = Usuario.query.filter_by(documento_u=dataEx['document']).first()
         if data != None:
             lineas = Linea.query.filter_by(documento_usuario=dataEx['document']).all()
             documento = []
@@ -23,3 +24,18 @@ class Change(MethodView):
         else:
             return jsonify({'state':'document'})
         return 'Updated line status'
+
+    def put(self):
+        dataEx = request.get_json()
+        numero = Linea.query.filter_by(numero_linea=dataEx['Numero de Linea']).first()
+        estado = numero.estado_linea
+        if estado == True:
+            numero.estado_linea = False
+            state = data_m.update()
+            return jsonify({'state':'inactivo'})
+        elif estado == False:
+            numero.estado_linea = True
+            state = data_m.update()
+            return jsonify({'state':'activo'})
+        else:
+            return jsonify({'state':'error'})
